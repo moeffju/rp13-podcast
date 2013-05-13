8f7a0c4512b1b5c32b6719a628b98534c0ef3f68
Matthias Bauer <moeffju@moeffju.net>
2013-05-13 03:03:32 +0200

    Ugly hack is a-go

---
+++ feed.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+import feedparser
+import urllib
+import re
+import sys
+import os
+from xml.etree.cElementTree import parse, tostring
+from werkzeug.contrib.atom import AtomFeed
+from datetime import datetime
+from uuid import uuid4
+from glob import glob
+
+
+base_url = 'https://moeffju.net/data/rp13'
+
+schedule = parse('rp13-schedule.xml')
+
+feed_urls = ['https://gdata.youtube.com/feeds/api/users/republica2010/uploads?max-results=50&start-index=%s' %
+             i for i in range(0, 100, 50)]
+feed_content = ''
+for url in feed_urls:
+    print >>sys.stderr, url
+    feed_content += urllib.urlopen(url).read()
+ytfeed = feedparser.parse(feed_content)
+
+feed = AtomFeed("re:publica 2013: Audio", feed_url="%s/audio.atom" % base_url,
+                subtitle="Audio feed of re:publica 2013 sessions")
+
+for entry in ytfeed.entries:
+    try:
+        event_id = re.search('Find out more at: http://13.re-publica.de/node/(\d+)', entry['summary']).group(1)
+        ytid = re.search('http://gdata.youtube.com/feeds/api/videos/(.*)', entry['id']).group(1)
+    except AttributeError as e:
+        print >>sys.stderr, "Skipping entry: %s" % entry['title']
+        continue
+    print >>sys.stderr, "ID %s / YTID %s: %s" % (event_id, ytid, entry['title'])
+
+    event = schedule.find('.//event[@id="%s"]' % event_id)
+    fns = glob('*%s.*' % ytid)
+    if len(fns) > 0:
+        print >>sys.stderr, "Matched: %s" % ', '.join(fns)
+    links = []
+    links.append({'href': "https://www.youtube.com/watch?v=%s" % ytid, 'rel': 'alternate', 'type': 'text/html'})
+    for fn in fns:
+        if fn[-3:] == 'ogg':
+            links.append({'href': "%s/%s" % (base_url, fn), 'rel': 'alternate',
+                         'type': 'audio/ogg; codecs=vorbis', 'length': os.path.getsize(fn)})
+        elif fn[-3:] == 'mp3':
+            links.append({'href': "%s/%s" % (base_url, fn), 'rel': 'alternate',
+                         'type': 'audio/mpeg', 'length': os.path.getsize(fn)})
+
+    try:
+        content = event.find('description').text.replace('&lt;', '<').replace('&gt;', '>').replace('&amp;', '&')
+        content = re.sub('<img[^>]*>', '(Image removed)', content)
+    except:
+        content = ''
+
+    try:
+        summary = event.find('abstract').text.replace('&lt;', '<').replace('&gt;', '>').replace('&amp;', '&')
+    except:
+        summary = ''
+
+    feed.add(title=event.find('title').text,
+             title_type='html',
+             content=content,
+             summary=summary,
+             id="tag:moeffju.net,%s:rp13,audio,%s,%s" % (datetime.strptime(entry['published'], '%Y-%m-%dT%H:%M:%S.000Z').strftime('%Y-%m-%d'), event_id, ytid),
+             author=[e.text for e in event.findall('.//person')],
+             rights="Creative Commons Attribution-ShareAlike 3.0 Germany (CC BY-SA 3.0 DE)",
+             updated=datetime.strptime(entry['updated'], '%Y-%m-%dT%H:%M:%S.000Z'),
+             published=datetime.strptime(entry['published'], '%Y-%m-%dT%H:%M:%S.000Z'),
+             links=links,
+             )
+
+print feed.to_string().encode('utf-8')
