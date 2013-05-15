1240e1a50d700bf2988fd6911d75d4645d6c0ebf
Matthias Bauer <moeffju@moeffju.net>
2013-05-15 16:29:36 +0200

    Reduce and extend

--- feed.py
+++ feed.py
@@ -7,47 +7,123 @@ import re
 import sys
 import os
 import subprocess
-from xml.etree.cElementTree import parse, tostring
+import json
+from xml.etree.cElementTree import parse, fromstring, tostring
 from werkzeug.contrib.atom import AtomFeed
+from progressbar import ProgressBar
 from datetime import datetime
 from glob import glob
 
 
 base_url = 'https://moeffju.net/data/rp13'
 
-schedule = parse('rp13-schedule.xml')
 
-feed_urls = ['https://gdata.youtube.com/feeds/api/users/republica2010/uploads?max-results=50&start-index=%s' %
-             i for i in range(0, 300, 50)]
-feed_content = ''
-for url in feed_urls:
-    print >>sys.stderr, url
-    feed_content += urllib.urlopen(url).read()
-ytfeed = feedparser.parse(feed_content)
+def get_schedule():
+    if os.path.exists('rp13-schedule.xml'):
+        print >>sys.stderr, "Loading rp13 schedule XML…"
+        schedule = parse(open('rp13-schedule.xml'))
+    else:
+        print >>sys.stderr, "Fetching rp13 schedule XML…"
+        schedule = fromstring(urllib.urlopen('http://re-publica.de/rp13-schedule.xml').read())
+    return schedule
+
+
+def get_known_videos():
+    if os.path.exists('knownvideos.json'):
+        print >>sys.stderr, "Loading list of known videos…"
+        known_videos = json.load(open('knownvideos.json'))
+    else:
+        print >>sys.stderr, "Fetching list of known videos…"
+        known_videos = json.loads(urllib.urlopen('http://michaelkreil.github.io/republicavideos/data/knownvideos.json').read())
+    return known_videos
+
+
+def get_youtube_feed(num_results=200):
+    print >>sys.stderr, "Fetching YouTube video feed…"
+    feed_urls = ['https://gdata.youtube.com/feeds/api/users/republica2010/uploads?max-results=50&start-index=%s' % i for i in range(0, num_results, 50)]
+    bar = ProgressBar(len(feed_urls)).start()
+    feed_content = ''
+    for url in feed_urls:
+        feed_content += urllib.urlopen(url).read()
+        bar.update(bar.currval+1)
+    ytfeed = feedparser.parse(feed_content)
+    bar.finish()
+    return ytfeed
+
+
+def get_youtube_video(video_id):
+    print >>sys.stderr, "[%s] loading video data" % video_id
+    video_url = 'http://gdata.youtube.com/feeds/api/videos/%s?v=2' % video_id
+    feed = feedparser.parse(urllib.urlopen(video_url).read())
+    return feed.entries[0]
+
+
+def get_session_info(session_id):
+    event = schedule.find('.//event[@id="%s"]' % event_id)
+
+    title = authors = summary = description = ''
+    try:
+        title = event.find('title').text.replace('&lt;', '<').replace('&gt;', '>').replace('&amp;', '&')
+        authors = [e.text for e in event.findall('.//person')]
+        summary = event.find('abstract').text.replace('&lt;', '<').replace('&gt;', '>').replace('&amp;', '&')
+        description = re.sub('<img[^>]*>', '(Image removed)', event.find(
+            'description').text.replace('&lt;', '<').replace('&gt;', '>').replace('&amp;', '&'))
+    except AttributeError:
+        pass
+
+    return {'description': description, 'summary': summary, 'authors': authors, 'title': title}
+
+
+def find_files_for_id(video_id):
+    return glob('*%s.mp3' % video_id) + glob('*%s.ogg' % video_id)
+
+
+def get_file_for_id(video_id):
+    print >>sys.stderr, "[%s] downloading file" % ytid
+    open('%s.mp3' % video_id, 'w')
+    return True
+    res = subprocess.call(
+        'youtube-dl --extract-audio --audio-format mp3 -c -f 18 -- "https://www.youtube.com/watch?v=%s" >&2' % ytid, shell=True)
+    return res
+
+
+youtube_feed = get_youtube_feed()
+known_videos = get_known_videos()
+schedule = get_schedule()
 
 feed = AtomFeed("re:publica 2013: Audio", feed_url="%s/audio.atom" % base_url,
                 subtitle="Audio feed of re:publica 2013 sessions")
 
-for entry in ytfeed.entries:
-    try:
-        event_id = re.search('Find out more at: http://13.re-publica.de/node/(\d+)', entry['summary']).group(1)
-        ytid = re.search('http://gdata.youtube.com/feeds/api/videos/(.*)', entry['id']).group(1)
-    except AttributeError as e:
-        print >>sys.stderr, "Skipping entry: %s" % entry['title']
+for ytid, data in known_videos.items():
+    if data == {}:
+        # print >>sys.stderr, "%s: no data, skipped" % ytid
         continue
-    print >>sys.stderr, "ID %s / YTID %s: %s" % (event_id, ytid, entry['title'])
+    if data['gesperrt']:
+        print >>sys.stderr, "[%s] RESTRICTED, skipping" % ytid
+        continue
 
-    event = schedule.find('.//event[@id="%s"]' % event_id)
-    fns = glob('*%s.mp3' % ytid) + glob('*%s.ogg' % ytid)
-    if len(fns) > 0:
-        print >>sys.stderr, "Matched: %s" % ', '.join(fns)
-    else:
-        print >>sys.stderr, "Downloading: %s" % ytid
-        subprocess.call('youtube-dl --extract-audio --audio-format mp3 -c -f 18 -- "https://www.youtube.com/watch?v=%s" >&2' % ytid, shell=True)
-        fns = glob('*%s.mp3' % ytid) + glob('*%s.ogg' % ytid)
+    event_id = data['eventId']
+
+    try:
+        video_info = [entry for entry in youtube_feed.entries if entry['id'][-11:] == ytid][0]
+    except IndexError:
+        video_info = get_youtube_video(ytid)
+
+    files = find_files_for_id(ytid)
+    session_info = get_session_info(event_id)
+
+    tag_tst = datetime.strptime(video_info['published'], '%Y-%m-%dT%H:%M:%S.000Z').strftime('%Y-%m-%d')
+
+    if len(files) == 0:
+        get_file_for_id(ytid)
+        files = find_files_for_id(ytid)
+    if len(files) == 0:
+        print >>sys.stderr, "[%s] ERROR getting file, skipping" % ytid
+        continue
+
     links = []
     links.append({'href': "https://www.youtube.com/watch?v=%s" % ytid, 'rel': 'alternate', 'type': 'text/html'})
-    for fn in fns:
+    for fn in files:
         if fn[-3:] == 'ogg':
             links.append({'href': "%s/%s" % (base_url, fn), 'rel': 'enclosure',
                          'type': 'audio/ogg; codecs=vorbis', 'length': os.path.getsize(fn)})
@@ -55,26 +131,15 @@ for entry in ytfeed.entries:
             links.append({'href': "%s/%s" % (base_url, fn), 'rel': 'enclosure',
                          'type': 'audio/mpeg', 'length': os.path.getsize(fn)})
 
-    try:
-        content = event.find('description').text.replace('&lt;', '<').replace('&gt;', '>').replace('&amp;', '&')
-        content = re.sub('<img[^>]*>', '(Image removed)', content)
-    except:
-        content = ''
-
-    try:
-        summary = event.find('abstract').text.replace('&lt;', '<').replace('&gt;', '>').replace('&amp;', '&')
-    except:
-        summary = ''
-
-    feed.add(title=event.find('title').text,
+    feed.add(title=session_info['title'],
              title_type='html',
-             content=content,
-             summary=summary,
-             id="tag:moeffju.net,%s:rp13,audio,%s,%s" % (datetime.strptime(entry['published'], '%Y-%m-%dT%H:%M:%S.000Z').strftime('%Y-%m-%d'), event_id, ytid),
-             author=[e.text for e in event.findall('.//person')],
+             content=session_info['description'],
+             summary=session_info['summary'],
+             id="tag:moeffju.net,%s:rp13,audio,%s,%s" % (tag_tst, event_id, ytid),
+             author=session_info['authors'],
              rights="Creative Commons Attribution-ShareAlike 3.0 Germany (CC BY-SA 3.0 DE)",
-             updated=datetime.strptime(entry['updated'], '%Y-%m-%dT%H:%M:%S.000Z'),
-             published=datetime.strptime(entry['published'], '%Y-%m-%dT%H:%M:%S.000Z'),
+             updated=datetime.strptime(video_info['updated'], '%Y-%m-%dT%H:%M:%S.000Z'),
+             published=datetime.strptime(video_info['published'], '%Y-%m-%dT%H:%M:%S.000Z'),
              links=links,
              )
 
